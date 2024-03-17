fcb3a13a957cafaad5648d0e164d79d61bd0de36
oliver <2080917477@qq.com>
2024-03-17 20:49:07 +0800

    docs: Validation-Calorie Counter comments

--- Validation-Calorie Counter/script.js
+++ Validation-Calorie Counter/script.js
@@ -1,27 +1,40 @@
-const calorieCounter = document.getElementById('calorie-counter');
-const budgetNumberInput = document.getElementById('budget');
-const entryDropdown = document.getElementById('entry-dropdown');
-const addEntryButton = document.getElementById('add-entry');
-const clearButton = document.getElementById('clear');
-const output = document.getElementById('output');
+// 变量
+const calorieCounter = document.getElementById("calorie-counter");
+const budgetNumberInput = document.getElementById("budget");
+const entryDropdown = document.getElementById("entry-dropdown");
+const addEntryButton = document.getElementById("add-entry");
+const clearButton = document.getElementById("clear");
+const output = document.getElementById("output");
 let isError = false;
 
+// 清除匹配字符串
 function cleanInputString(str) {
+  // 全局匹配 任何加号、减号或空格字符
   const regex = /[+-\s]/g;
-  return str.replace(regex, '');
+  return str.replace(regex, "");
 }
 
+// 输入是否非法
 function isInvalidInput(str) {
+  // 匹配任何形式的科学计数法表示的数字
   const regex = /\d+e\d+/i;
   return str.match(regex);
 }
 
+// 增加条目
 function addEntry() {
-  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+  // 1. 定位输入容器
+  const targetInputContainer = document.querySelector(
+    `#${entryDropdown.value} .input-container`
+  );
+  // 2. 计算填入个数：开始没有输入框为0，+1表示目前的输入框（HTML String中的第一个input）
+  const entryNumber =
+    targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+  // 3. 生成HTML
   const HTMLString = `
   <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
   <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+
   <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
   <input
     type="number"
@@ -29,19 +42,32 @@ function addEntry() {
     id="${entryDropdown.value}-${entryNumber}-calories"
     placeholder="Calories"
   />`;
-  targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
+  // 4. 将一个 HTML 字符串动态地插入到指定元素的末尾。
+  targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
 }
 
+// 计算卡路里
 function calculateCalories(e) {
+  // 防止事件的默认行为发生（提交表单不刷新）
   e.preventDefault();
   isError = false;
-
-  const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
-  const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
-  const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');
-  const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
-  const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
-
+  // NodeList
+  const breakfastNumberInputs = document.querySelectorAll(
+    "#breakfast input[type=number]"
+  );
+  const lunchNumberInputs = document.querySelectorAll(
+    "#lunch input[type=number]"
+  );
+  const dinnerNumberInputs = document.querySelectorAll(
+    "#dinner input[type=number]"
+  );
+  const snacksNumberInputs = document.querySelectorAll(
+    "#snacks input[type=number]"
+  );
+  const exerciseNumberInputs = document.querySelectorAll(
+    "#exercise input[type=number]"
+  );
+  // number
   const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
   const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
   const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
@@ -52,25 +78,34 @@ function calculateCalories(e) {
   if (isError) {
     return;
   }
+  // 计算每部分卡路里
+  const consumedCalories =
+    breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
 
-  const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
-  const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
-  const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
+  const remainingCalories =
+    budgetCalories - consumedCalories + exerciseCalories;
+  // 多了还是少了
+  const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit";
+  // 生成输出结果
   output.innerHTML = `
-  <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+  <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(
+    remainingCalories
+  )} Calorie ${surplusOrDeficit}</span>
   <hr>
   <p>${budgetCalories} Calories Budgeted</p>
   <p>${consumedCalories} Calories Consumed</p>
   <p>${exerciseCalories} Calories Burned</p>
   `;
-
-  output.classList.remove('hide');
+  // 修改css样式，使结果显示
+  output.classList.remove("hide");
 }
 
+// 将传入的NodeList计算为数值
 function getCaloriesFromInputs(list) {
   let calories = 0;
 
   for (const item of list) {
+    // 当前值是否合法
     const currVal = cleanInputString(item.value);
     const invalidInputMatch = isInvalidInput(currVal);
 
@@ -79,23 +114,30 @@ function getCaloriesFromInputs(list) {
       isError = true;
       return null;
     }
+    // 合法则计入
     calories += Number(currVal);
   }
   return calories;
 }
 
+// 清除表单内容
 function clearForm() {
-  const inputContainers = Array.from(document.querySelectorAll('.input-container'));
-
+  // 选中所有输入框
+  const inputContainers = Array.from(
+    document.querySelectorAll(".input-container")
+  );
+  // 清除输入框的所有内容
   for (const container of inputContainers) {
-    container.innerHTML = '';
+    container.innerHTML = "";
   }
 
-  budgetNumberInput.value = '';
-  output.innerText = '';
-  output.classList.add('hide');
+  budgetNumberInput.value = "";
+  // 修改css，让输出部分消失
+  output.innerText = "";
+  output.classList.add("hide");
 }
 
+// 添加监听事件 （任何地方都行）
 addEntryButton.addEventListener("click", addEntry);
 calorieCounter.addEventListener("submit", calculateCalories);
-clearButton.addEventListener('click', clearForm);
+clearButton.addEventListener("click", clearForm);
--- Validation-Calorie Counter/summary.md
+++ Validation-Calorie Counter/summary.md
@@ -1,1 +1,28 @@
 The difference between innerText and innerHTML is that innerText will not render HTML elements, but will display the tags and content as raw text.
+
+```js
+const targetInputContainer = document.querySelector(
+  `#${entryDropdown.value} .input-container`
+);
+```
+
+模板字符串 (Template Literals) ，动态获取目标输入容器元素
+
+```javascript
+// 修改css样式，使结果显示/消失
+output.classList.remove("hide");
+output.classList.add("hide");
+```
+
+```html
+<div id="output" class="output hide"></div>
+```
+
+修改 css 样式，使结果显示/消失
+
+```js
+addEntryButton.addEventListener("click", addEntry);
+calorieCounter.addEventListener("submit", calculateCalories);
+```
+
+添加监听事件 （任何地方都行）
